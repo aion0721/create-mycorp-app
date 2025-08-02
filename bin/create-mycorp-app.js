3ff5568f60437e37dc92c577457387d8e92e9546
aion0721 <rudolf.aion@gmail.com>
2025-08-02 17:56:37 +0900

    fix: iife

--- bin/create-mycorp-app.js
+++ bin/create-mycorp-app.js
@@ -7,21 +7,26 @@ import inquirer from "inquirer";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-let [, , projectName] = process.argv;
+(async () => {
+  let [, , projectName] = process.argv;
 
-if (!projectName) {
-  const answer = await inquirer.prompt([
-    {
-      type: "input",
-      name: "projectName",
-      message: "プロジェクト名を入力してね💕",
-      default: "mycorp-app",
-    },
-  ]);
-  projectName = answer.projectName;
-}
+  if (!projectName) {
+    const answer = await inquirer.prompt([
+      {
+        type: "input",
+        name: "projectName",
+        message: "プロジェクト名を入力してね💕",
+        default: "mycorp-app",
+      },
+    ]);
+    projectName = answer.projectName;
+  }
 
-const plop = await nodePlop(resolve(__dirname, "../plopfile.js"));
-const generator = plop.getGenerator("default");
+  const plop = await nodePlop(resolve(__dirname, "../plopfile.js"));
+  const generator = plop.getGenerator("default");
 
-await generator.runActions({ projectName });
+  const result = await generator.runActions({ projectName });
+
+  // 👀 ログで確認したいとき
+  // console.log(result);
+})();
